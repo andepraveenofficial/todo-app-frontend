6b21f80a86dc798ac31cf0606aca6a0312dbb624
Ande Praveen <praveenande2023@gmail.com>
2024-11-14 08:02:52 +0530

    API added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 // src/services/api.ts
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1';
+// const API_URL = 'http://localhost:5000/api/v1';
+const API_URL = 'https://todo-app-backend-wcmc.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -10,4 +11,4 @@ const api = axios.create({
   },
 });
 
-export {api}
+export { api };
